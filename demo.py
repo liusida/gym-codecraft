650b7ffeae8d28807bcccd89ddf03eba1d18ab64
Sida Liu <learner.sida.liu@gmail.com>
2023-06-15 23:29:16 +0800

    different types of actions

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
+import json
 import gymnasium
 import gym_codecraft
 
-# WARNING: Clean up any existing containers
+# WARNING: Clean up any existing containers (There's always too many stopped containers on my machine:)
 import docker
 docker.from_env().containers.prune()
 
@@ -9,9 +10,20 @@ env = gymnasium.make("gym_codecraft/CodeCraft-v0")
 obs = env.reset()
 print(obs)
 
-commands = ['pwd', 'echo \'print("Hello, world!")\' > hello.py', 'cat hello.py', 'python hello.py']
-for command in commands:
-    obs, reward, _, _, _ = env.step(['/bin/sh', '-c', command])
+actions = [
+    {'action':'lol'},
+    {'action':'command', 'command':'pwd'},
+    {'action':'write_file', 'path':'hello.py', 'content': 'print("Hello, world!")'},
+    {'action':'command', 'command':'ls'},
+    {'action':'command', 'command':'cat hello.py'},
+    {'action':'command', 'command':'python hello.py'},
+    {'action':'submit'}
+]
+for action in actions:
+    print(action)
+    obs, reward, _, _, _ = env.step(json.dumps(action))
     print(obs)
+    if (reward != 0):
+        print(f"Reward: {reward}")
 
 env.close()
--- gym_codecraft/envs/codecraft.py
+++ gym_codecraft/envs/codecraft.py
@@ -3,6 +3,8 @@ from gymnasium import spaces
 import string
 import json
 import docker
+import tarfile
+import io
 
 class CodeCraftEnv(gym.Env):
     def __init__(self):
@@ -10,6 +12,7 @@ class CodeCraftEnv(gym.Env):
         self.action_space = spaces.Text(10, charset=string.printable)
         self.client = docker.from_env()
         self.container = None
+        self.working_dir = "/"
 
     def reset(self, task_id="1", seed=None, options=None):
         # TODO: read curriculum.json to get the task
@@ -19,20 +22,56 @@ class CodeCraftEnv(gym.Env):
             task = curriculum_data['tasks'][task_id]
             docker_image = task['docker']
             shell = task['shell']
-            working_dir = task['working_dir']
-            self.container = self.client.containers.run(docker_image, command=shell, working_dir=working_dir, detach=True, tty=True, remove=True)
+            self.working_dir = task['working_dir']
+            self.container = self.client.containers.run(docker_image, command=shell, working_dir=self.working_dir, detach=True, tty=True, remove=True)
             return {"obs": f"Task {task_id}:\n {task}\n"}, {}
 
         else:
             return {"obs": f"Task {task_id} not found.\n"}, {}
 
-    def step(self, action):
-        exec_result = self.container.exec_run(action) # type: ignore
-
+    def step(self, action : str):
         terminated = False
-        reward = 1 if terminated else 0  # Binary sparse rewards
-        observation = {"obs": exec_result.output.decode('utf-8')}
         info = {"info": ""}
+        observation = {"obs": ""}
+        reward = 0
+        act = None
+        try:
+            act = json.loads(action)
+        except:
+            reward = -1
+            observation = {"obs": "Invalid action"}
+
+        if act:
+            # 1. Shell Command
+            if act['action'] == 'command':
+                exec_result = self.container.exec_run(act['command']) # type: ignore
+                observation = {"obs": exec_result.output.decode('utf-8')}
+            
+            # 2. Write a File
+            elif act['action'] == 'write_file':
+                container_dest_path = act['path']
+                file_content = act['content']
+                tar_buffer = io.BytesIO()
+                tar = tarfile.open(fileobj=tar_buffer, mode='w')
+                tarinfo = tarfile.TarInfo(name=container_dest_path)
+                tarinfo.size = len(file_content)
+                tar.addfile(tarinfo, io.BytesIO(file_content.encode('utf-8')))
+                tar.close()
+                tar_bytes = tar_buffer.getvalue()
+                # Copy the tarball to the container
+                self.container.put_archive(path=self.working_dir, data=tar_bytes) # type: ignore
+                observation = {"obs": f"File {container_dest_path} written."}
+
+            # 3. Submit the code
+            elif act['action'] == 'submit':
+                # TODO: check the submission, give reward = 1 if correct
+                reward = 1
+                observation = {"obs": "Code submitted."}
+
+            # 4. Unknown action
+            else:
+                reward = -1
+                observation = {"obs": f"Unknown action: {act['action']}"}
 
         return observation, reward, terminated, False, info
     
